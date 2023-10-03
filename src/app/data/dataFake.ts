c7cc8111fc7d3bf508419e93c056b98ecb1c2e9c
ancgci <sr.antoniocarlos@gmail.com>
2023-10-03 15:47:25 -0300

    atualização de componentes

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+ {
+  "id":"1",
+  "title": "Construindo um blog com Angular teste",
+  "description": "Desenvolvendo um projeto angular com bootcamp FullStack Mesmo",
+  "photoCover" : "https://logowik.com/content/uploads/images/google-bard3872.logowik.com.webp"
+ },
+ {
+  "id":"2",
+  "title": "Angular é um framework",
+  "description": "Maximiza o desenvolvimento frontend",
+  "photoCover" : "https://logowik.com/content/uploads/images/angular9826.logowik.com.webp"
+ }
+]
